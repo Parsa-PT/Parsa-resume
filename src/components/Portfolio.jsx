a3066528173d55d8d63bce72a7f3e58888ebda20
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2023-01-22 22:37:37 +0330

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,25 +12,29 @@ const Portfolio = () => {
       id: 1,
       title : 'Calculator',
       url : fristpic,
-      address : 'https://github.com/Parsa-PT/calculator'
+      address : 'https://github.com/Parsa-PT/calculator',
+      liveMode: 'https://calculatorapp.w3spaces.com/'
     },
     {
       id: 2,
       title : 'Qrcode',
       url : midpic,
-      address : 'https://github.com/Parsa-PT/Qrcode'
+      address : 'https://github.com/Parsa-PT/Qrcode',
+      liveMode: 'https://qr-code-generator.w3spaces.com/'
     },
     {
       id: 3,
       title : 'Tech-finder',
       url : latspic,
-      address : 'https://github.com/Parsa-PT/tech-finder'
+      address : 'https://github.com/Parsa-PT/tech-finder',
+      liveMode: 'https://pouyandev1.pythonanywhere.com'
     },
     {
       id: 4,
       title : 'Visit card',
       url : imanipic,
-      address : 'https://github.com/Parsa-PT/imanishop.github'
+      address : 'https://github.com/Parsa-PT/imanishop.github',
+      liveMode: 'https://stirring-daffodil-6edb36.netlify.app/'
     },
   ]
   return (
@@ -43,7 +47,11 @@ const Portfolio = () => {
               <div className='w-full overflow-hidden rounded-lg  gap-4 flex flex-col items-center bg-[#2d0c658f]'>
                   <img className=' hover:scale-105 duration-150' src={item.url} alt="mypic" />
                   <p className='text-white'>{item.title}</p>
-                  <a target={'_blank'} rel="noreferrer" className='btn-bg w-[10rem] mb-3 text-white px-4 py-2 rounded-md flex  justify-center' href={item.address}>GitHub </a>
+                  <div className='lg:flex lg:gap-4 px-3'>
+                      <a target={'_blank'} rel="noreferrer" className='btn-bg w-[10rem] mb-3 text-white px-4 py-2 rounded-md flex  justify-center' href={item.address}>GitHub </a>
+                      <a target={'_blank'} rel="noreferrer" className='btn-bg w-[10rem] mb-3 text-white px-4 py-2 rounded-md flex  justify-center' href={item.liveMode}>Live </a>
+                  </div>
+                
               </div>
             )
           })}
